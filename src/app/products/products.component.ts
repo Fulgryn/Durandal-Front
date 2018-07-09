8cb82369687b1fd4734f4fb3bf255b7704bcfb67
DocThomus <bonninthom2@gmail.com>
2018-07-09 16:48:18 +0200

    commit avant de pull master (cart)

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -5,76 +5,78 @@ import { SelectItem } from 'primeng/api';
 import { CartService } from '../cart.service';
 
 @Component({
-  selector: 'app-products',
-  templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+    selector: 'app-products',
+    templateUrl: './products.component.html',
+    styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
 
 
-  @Input() admin: boolean;
-  products: Array<Product>;
+    @Input() admin: boolean;
+    products: Array<Product>;
 
-  selectedProduct: Product;
+    selectedProduct: Product;
 
-  displayDialog: boolean;
-  displayDialog2: boolean;
+    displayDialog: boolean;
+    displayDialog2: boolean;
 
-  sortOptions: SelectItem[];
+    sortOptions: SelectItem[];
 
-  sortKey: string;
+    sortKey: string;
 
-  sortField: string;
+    sortField: string;
 
-  sortOrder: number;
+    sortOrder: number;
 
 
-  constructor(private productService: ProductService, private cartService : CartService) {
-    this.productService = productService;
-    this.products = [];
-  }
-
-  ngOnInit() {
-    this.productService.getProducts().subscribe(myProds => this.products = myProds);
-
-    this.sortOptions = [
-      { label: 'Nom', value: 'name' },
-      { label: 'Prix: le moins cher', value: 'price' },
-      { label: 'Prix: le plus cher', value: '!price' },
-      { label: 'Editeur', value: 'editor' }
-    ];
-  }
-
-  selectProduct(event: Event, product: Product) {
-    this.selectedProduct = product;
-    this.displayDialog = true;
-    event.preventDefault();
-  }
-
-  addToCart(product: Product) {
-      this.cartService.addToCart(product, 1);
-  }
-
-  onSortChange(event) {
-    const value = event.value;
-
-    if (value.indexOf('!') === 0) {
-      this.sortOrder = -1;
-      this.sortField = value.substring(1, value.length);
-    } else {
-      this.sortOrder = 1;
-      this.sortField = value;
+    constructor(private productService: ProductService, private cartService: CartService) {
+        this.productService = productService;
+        this.products = [];
     }
-  }
 
-  onDialogHide() {
-    this.selectedProduct = null;
-  }
+    ngOnInit() {
+        this.productService.getProducts().subscribe(myProds => this.products = myProds);
+
+        this.sortOptions = [
+            { label: 'Nom', value: 'name' },
+            { label: 'Prix: le moins cher', value: 'price' },
+            { label: 'Prix: le plus cher', value: '!price' },
+            { label: 'Editeur', value: 'editor' }
+        ];
+    }
+
+    selectProduct(event: Event, product: Product) {
+        this.selectedProduct = product;
+        this.displayDialog = true;
+        event.preventDefault();
+    }
+
+    addToCart(product: Product) {
+        this.cartService.addToCart(product, 1);
+    }
+
+    onSortChange(event) {
+        const value = event.value;
+
+        if (value.indexOf('!') === 0) {
+            this.sortOrder = -1;
+            this.sortField = value.substring(1, value.length);
+        } else {
+            this.sortOrder = 1;
+            this.sortField = value;
+        }
+    }
+
+    onDialogHide() {
+        this.selectedProduct = null;
+    }
 
 
-  delProduct(event: Event, product: Product) {
-    this.selectedProduct = product;
-    this.displayDialog2 = true;
-    event.preventDefault();
-  }
+    delProduct(event: Event, product: Product) {
+        this.selectedProduct = product;
+        this.displayDialog2 = true;
+        event.preventDefault();
+    }
 }
+
+
